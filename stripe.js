ba628425767762ed0d1543f44997639a73f39bc8
Abde lilah <85647797+0xGLSS@users.noreply.github.com>
2025-04-13 17:21:23 +0100

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -1,1 +1,119 @@
-faf
+// ========== BASIC PATTERNS ==========
+
+// API Endpoint
+const apiEndpoint = "https://api.example.com/v1/";
+
+// URL Parameter
+const urlWithParam = "https://example.com/data?query=value";
+
+// JavaScript URL
+const jsFileUrl = "https://cdn.example.com/script.js";
+
+// ========== PREMIUM PATTERNS ==========
+
+// GitHub Token
+const githubToken = "ghp_abcdefghijklmnopqrstuvwxyzABCDEF123456";
+
+// Google API Key
+const googleApiKey = "AIzaSyA1234567890abcdefghijklmnOPQRST";
+
+// JWT Token
+const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+// Slack Token
+const slackToken = "xoxb-123456789012-123456789012-123456789012-abcdefghijklmnopqrstuvwx";
+
+// Amazon MWS Auth Token
+const amazonMwsToken = "amzn.mws.12345678-1234-1234-1234-123456789012";
+
+// Facebook Access Token
+const fbAccessToken = "EAACEdEose0cBA1234567890abcdefg";
+
+// Facebook OAuth
+const fbOAuth = "facebook = '0123456789abcdef0123456789abcdef'";
+
+// GitHub Auth
+const githubAuth = "github = 'abcdefghijklmnopqrstuvwxyz1234567890123456'";
+
+// Heroku API Key
+const herokuKey = "heroku = 12345678-1234-1234-1234-1234567890AB";
+
+// MailChimp API Key
+const mailchimpKey = "0123456789abcdef0123456789abcdef-us6";
+
+// Mailgun API Key
+const mailgunKey = "key-1234567890abcdef1234567890abcdef";
+
+// PayPal Braintree Access Token
+const paypalToken = "access_token$production$abcdefgh12345678$1234567890abcdef1234567890abcdef";
+
+// Picatic API Key
+const picaticKey = "sk_live_1234567890abcdef1234567890abcdef";
+
+// Slack Webhook
+const slackWebhook = "https://hooks.slack.com/services/T1234567/B1234567/AbCdEfGhIjKlMnOpQrStUvWx";
+
+// Stripe API Key
+const stripeKey = "sk_live_1234567890abcdef12345678";
+
+// Stripe Restricted API Key
+const stripeRestricted = "rk_live_AbCdEfGhIjKlMnOpQrStUvWx";
+
+// Square Access Token
+const squareToken = "sq0atp-1234567890abcdefghijklmn";
+
+// Square OAuth Secret
+const squareSecret = "sq0csp-1234567890abcdefghijklmnopqrstuvwxYZa";
+
+// Telegram Bot API Key
+const telegramKey = "123456:AAAbCdEfGhIjKlMnOpQrStUvWxYz123456789";
+
+// Twilio API Key
+const twilioKey = "SKabcdef1234567890abcdef1234567890";
+
+// Github Auth Creds
+const githubCreds = "https://0123456789abcdef0123456789abcdef01234567@github.com";
+
+// Private Key
+const privateKey = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASC...
+-----END PRIVATE KEY-----`;
+
+// SSH Key
+const sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtestsshkey==";
+
+// RSA Private Key
+const rsaKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEArandomRSAkey...
+-----END RSA PRIVATE KEY-----`;
+
+// DSA Private Key
+const dsaKey = `-----BEGIN DSA PRIVATE KEY-----
+MIIBuwIBAAKBgQDg...
+-----END DSA PRIVATE KEY-----`;
+
+// EC Private Key
+const ecKey = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIO...
+-----END EC PRIVATE KEY-----`;
+
+// PGP Private Key Block
+const pgpKey = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+Version: GnuPG v1
+...
+-----END PGP PRIVATE KEY BLOCK-----`;
+
+// MongoDB URI
+const mongoURI = "mongodb+srv://user:pass@cluster0.mongodb.net/mydatabase";
+
+// MySQL Connection
+const mysqlConn = "mysql://user:pass@localhost:3306/database";
+
+// PostgreSQL Connection
+const postgresConn = "postgresql://user:pass@localhost:5432/database";
+
+// Basic Auth
+const basicAuth = "basic dXNlcm5hbWU6cGFzc3dvcmQ=";
+
+// Password in URL
+const pwInUrl = "https://user:secretpass@host.com/resource";
