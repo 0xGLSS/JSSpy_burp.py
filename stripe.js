2fdb37cc2caf670b643950ebbaf89e7752718846
Abde lilah <85647797+0xGLSS@users.noreply.github.com>
2025-04-13 11:32:39 +0100

    Update stripe.js

--- stripe.js
+++ stripe.js
@@ -2,6 +2,5 @@ const config = {
   gateway: 'stripe',
   mode: 'production',
   debug: process.env.NODE_ENV === 'development',
-  stripe_key: 'sk_live_VePHdqKTYQjKNInc7u56JBrQ',
-  webhook_secret: 'whsec_1234567890abcdef1234567890abcdef'
+
 }g
